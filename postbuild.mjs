c109710ee45cb7c869030eeea29cf3e2f01b5f5b
Katarina Josipović <kjosipovic@vub.hr>
2026-08-30 16:17:48 +0200

    updated index.html

--- postbuild.mjs
+++ postbuild.mjs
@@ -1,11 +1,11 @@
 import fs from "fs";
 
-// kopiraj index.html u dist i promijeni base href za GitHub Pages podputanju
 let html = fs.readFileSync("index.html", "utf8");
-html = html.replace('<base href="/">', '<base href="/RecipeBox/">');
+html = html
+  .replace('<base href="/">', '<base href="/RecipeBox/">')
+  .replace('src="dist/bundle.js"', 'src="bundle.js"');
 fs.writeFileSync("dist/index.html", html);
 
-// kopiraj CSS i ikonu na iste relativne putanje unutar dist
 fs.mkdirSync("dist/src/assets", { recursive: true });
 fs.copyFileSync("src/style.css", "dist/src/style.css");
 fs.copyFileSync("src/assets/RecipeBoxLogo.svg", "dist/src/assets/RecipeBoxLogo.svg");
